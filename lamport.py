a6e3ab42fc6bd3a3cb03b142acd30eb7e81bf80f
Esdras R. Carmo <esdras.carmo@vtex.com.br>
2021-01-07 23:17:00 -0300

    lamport topzera

--- lamport.py
+++ lamport.py
@@ -1,132 +1,155 @@
 import redis
-import sys
 import json
 
-redis_host = '127.0.0.1'
-redis_port = 6379
 
-# Available nodes in for naming in the cluster
-available_nodes = set(['node1', 'node2', 'node3'])
-
-# The logical timestamp of this node
-timestamp = 0
-# The name used to send messages to this node
-node_name = None
-
-print("Choose from these node options:")
-for name in available_nodes:
-    print("\t-{}".format(name))
-
-input_name = ""
-while input_name not in available_nodes:
-    input_name = input("Type a valid name for this node: ")
-node_name = input_name
+_REDIS_HOST = '127.0.0.1'
+_REDIS_PORT = 6379
+_LOCK_TIMEOUT = 5
 
 
-def increment_timestamp(event_timestamp=0):
-    """ Updates the timestamp to the event timestamp if it is bigger and
-    increments the timestamp by one
+class Node:
+    def __init__(self, redis_cli, name):
+        self._r = redis_cli
+        self._node_name = name
+        # The logical timestamp of this node
+        self._timestamp = 0
 
-    Args:
-        event_timestamp (int, optional): The event_timestamp. Defaults to 0.
-    """
-    global timestamp
-    timestamp = max([timestamp, event_timestamp]) + 1
-    print("Timestamp is now:", timestamp)
+        with self._r.lock('nodes-lock', blocking_timeout=_LOCK_TIMEOUT):
+            self._r.lpush('nodes', self._node_name)
+
+    def __del__(self):
+        with self._r.lock('nodes-lock', blocking_timeout=_LOCK_TIMEOUT):
+            self._r.lrem('nodes', 0, self._node_name)
+
+    @property
+    def name(self):
+        return self._node_name
+
+    @property
+    def timestamp(self):
+        return self._timestamp
+        
+    def _increment_timestamp(self, event_timestamp=0):
+        """ Updates the timestamp to the event timestamp if it is bigger and
+        increments the timestamp by one
+
+        Args:
+            event_timestamp (int, optional): The event_timestamp. Defaults to 0.
+        """
+        self._timestamp = max([self._timestamp, event_timestamp]) + 1
+        print("Timestamp is now:", self._timestamp)
+
+    def _run_middleware(self, message):
+        """ Simulates a middleware in the application layer, gets a message
+        and operate over the headers, returning only the data to the original
+        function
+
+        Args:
+            message (dict): The message that comes directly from the redis lib
+
+        Raises:
+            ValueError: If a value error is raised, the messaged is supposed to be
+            ignored
+
+        Returns:
+            dict: The data without the middleware headers
+        """
+        data = message["data"]
+        parsed_data = {}
+        try:
+            parsed_data = json.loads(data)
+        except json.JSONDecodeError:
+            print("Received a message not in JSON format, ignoring it: ", data)
+            raise ValueError('Ignore Message')
+        sender = parsed_data["sender"]
+        if sender == self.name:
+            print("Independent event happened in this node!")
+        else:
+            print("Received message from ", sender)
+            self._increment_timestamp(parsed_data["timestamp"])
+
+        return parsed_data["data"]
+
+    def receive_direct_message(self, message):
+        """ Handles direct messsages for this node, i.e messages in the {node_name}
+        channel in redis
+
+        Args:
+            message (dict): Message from another node, this comes from the redis lib
+        """
+        try:
+            self._run_middleware(message)
+        except ValueError:
+            return
+
+    def receive_broadcast_message(self, message):
+        """ Handles messages that were broadcastes to all nodes, i.e messages in the "all"
+        channel in redis
+
+        Args:
+            message (dict): Message from another node, this comes from the redis lib
+        """
+        try:
+            self._run_middleware(message)
+        except ValueError:
+            return
+
+    def send_message(self, target, data):
+        """ Wrapper that publishes a message on a target(channel in redis).
+        Increments logical timestamp by one
+        Args:
+            target (string): Target (channel in redis) that this message is directed to
+            data (dict): Data that is going to be sent with the messsage (normally has a 'type' field to categorize it)
+        """
+        self._r.publish(target, json.dumps({
+            "timestamp": self.timestamp,
+            "sender": self.name,
+            "data": data,
+        }))
+        print("Sent message to", target)
+        self._increment_timestamp()
 
 
-def run_middleware(message):
-    """ Simulates a middleware in the application layer, gets a message
-    and operate over the headers, returning only the data to the original
-    function
+def _get_available_nodes(r):
+    with r.lock('nodes-lock', blocking_timeout=_LOCK_TIMEOUT):
+        nodes = r.lrange('nodes', 0, -1)
+        if not nodes:
+            nodes = []
+        return [node.decode('utf8') for node in nodes]
 
-    Args:
-        message (dict): The message that comes directly from the redis lib
 
-    Raises:
-        ValueError: If a value error is raised, the messaged is supposed to be
-        ignored
+def main():
+    r = redis.Redis(host=_REDIS_HOST, port=_REDIS_PORT)
 
-    Returns:
-        dict: The data without the middleware headers
-    """
-    global timestamp
-    global node_name
-    data = message["data"]
-    parsed_data = {}
+    input_name = input("Type a name for this node: ")
+    node = Node(r, input_name)
+
+    p = r.pubsub()
+    p.subscribe(**{
+        "all": node.receive_broadcast_message,
+        node.name: node.receive_direct_message
+    })
+    p.run_in_thread(sleep_time=0.001)
+
     try:
-        parsed_data = json.loads(data)
-    except json.JSONDecodeError:
-        print("Received a message not in JSON format, ignoring it: ", data)
-        raise ValueError('Ignore Message')
-    sender = parsed_data["sender"]
-    if sender == node_name:
-        print("Independent event happened in this node!")
-    else:
-        print("Received message from ", sender)
-        increment_timestamp(parsed_data["timestamp"])
-
-    return parsed_data["data"]
-
-
-def receive_direct_message(message):
-    """ Handles direct messsages for this node, i.e messages in the {node_name}
-    channel in redis
-
-    Args:
-        message (dict): Message from another node, this comes from the redis lib
-    """
-    try:
-        run_middleware(message)
-    except ValueError:
-        return
+        print("""
+        Event loop started. Type the name of a node to send a event.
+        If you type the name of this node({}), then a independent event for this node will happen.
+        Type 'timestamp' for the current logical timestamp.
+        List of nodes:""".format(node.name))
+        for name in _get_available_nodes(r):
+            print("\t-{}".format(name))
+        while True:
+            input_command = input(f'nodes{_get_available_nodes(r)}: ')
+            if input_command in _get_available_nodes(r):
+                node.send_message(input_command, {})
+            elif input_command == 'timestamp':
+                print("Current timestamp is:", node.timestamp)
+    finally:
+        del node
+        p.unsubscribe()
+        p.close()
 
 
-def receive_broadcast_message(message):
-    """ Handles messages that were broadcastes to all nodes, i.e messages in the "all"
-    channel in redis
-
-    Args:
-        message (dict): Message from another node, this comes from the redis lib
-    """
-    try:
-        run_middleware(message)
-    except ValueError:
-        return
-
-def send_message(target, data):
-    """ Wrapper that publishes a message on a target(channel in redis).
-    Increments logical timestamp by one
-    Args:
-        target (string): Target (channel in redis) that this message is directed to
-        data (dict): Data that is going to be sent with the messsage (normally has a 'type' field to categorize it)
-    """
-    global r
-    r.publish(target, json.dumps({
-        "timestamp": timestamp,
-        "sender": node_name,
-        "data": data,
-    }))
-    print("Sent message to", target)
-    increment_timestamp()
-
-
-r = redis.Redis(host=redis_host, port=redis_port)
-p = r.pubsub()
-p.subscribe(**{"all": receive_broadcast_message,
-               node_name: receive_direct_message})
-thread = p.run_in_thread(sleep_time=0.001)
-
-print("""
-Event loop started. Type the name of a node to send a event.
-If you type the name of this node({}), then a independent event for this node will happen.
-Type 'timestamp' for the current logical timestamp.
-List of nodes:""".format(node_name))
-for name in available_nodes:
-    print("\t-{}".format(name))
-while True:
-    input_command = input()
-    if input_command in available_nodes:
-        send_message(input_command, {})
-    elif input_command == 'timestamp':
-        print("Current timestamp is:", timestamp)
+if __name__ == '__main__':
+    main()
